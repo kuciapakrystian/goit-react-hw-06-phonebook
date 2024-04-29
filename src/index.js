e1ef004ed11d16ef8bd3c5c3c14958635dad4cf6
kuciapakrystian <kuciapakrystian@outlook.com>
2024-04-29 16:37:37 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { ThemeProvider } from 'styled-components';
+import { App } from 'components/App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import { theme } from 'styles/theme';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { contactsReducer } from "./contactSlice";
+// If using Redux-Persist, you should specifically ignore all the action types it dispatches
+// https://redux-toolkit.js.org/usage/usage-guide#working-with-non-serializable-data
+import { configureStore } from '@reduxjs/toolkit';
 import {
   persistStore,
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
   PERSIST,
   PURGE,
   REGISTER,
-} from "redux-persist";
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+import { rootReducer } from './root-reducer';
+
+// об'єкт налаштувань, в якому записані, які дані зберігати в Local Storage
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts'],
+  // blacklist: ["filter"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { contacts: contactsReducer },
-  middleware: (getDefaultMiddleware) =>
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
@@ -20,4 +35,5 @@ export const store = configureStore({
     }),
 });
 
+// дані з локал сторіджа потрапляли відразу в redux під час завантаження
 export const persistor = persistStore(store);
